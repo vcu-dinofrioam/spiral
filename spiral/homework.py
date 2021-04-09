89c833c2cec6424d930ae9c1d5aadf444cfc0037
vcu-dinofrioam <78315569+vcu-dinofrioam@users.noreply.github.com>
2021-04-09 16:25:52 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,15 @@
 def spiralize(number):
     return_value = 1
+    number*=number
+    num=1
+    add=2
+    result=1
+    while num<number:
+        for i in range(4):
+            num+=add
+            result+=num
+        add+=2
+    return result
+print (spiralize(501))
 
     return return_value
